e7410c7b0024299a9aeef9171adf5467fae02262
cyphix2022 <jonahyiga02@gmail.com>
2024-04-06 09:51:08 -0700

    After some more changes

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,21 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute"
+
+
 
 
 //import express from "express";
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(()=> console.log("connected to database"));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 
 const app = expres();
@@ -20,7 +30,8 @@ app.get("/health", async (req: Request, res: Response)=>{
 
 })
 
-app.use("/api/my/user", myUserRoute)
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant",myRestaurantRoute )
 
 
 
